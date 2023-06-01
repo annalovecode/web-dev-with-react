b4119ddcc1be368c59f4ace7ddb793b98988eb71
annaz <46110477+annalovecode@users.noreply.github.com>
2023-05-31 21:41:55 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  //exercise1: increase the count number
+  const increaseHandler = () => {
+    setCount(count + 1);
+  };
+  //change doc title
+  useEffect(() => {
+    document.title = "change title" + String(count);
+  }, [count]);
   return (
     <div className='App'>
       <div className='section section-1'>
@@ -12,8 +21,8 @@ function App() {
           <p>Use react hooks: on every click of the 'Press me!' button, increment the number next to it by 1, also update the website title by setting document.title</p>
         </div>
         <div className='section-controls-container'>
-          <button>Press me!</button>
-          <span>0</span>
+          <button onClick={increaseHandler}>Press me!</button>
+          <span>{count}</span>
         </div>
       </div>
       <div className='section section-2'>
@@ -25,10 +34,10 @@ function App() {
         <div className='section-controls-container'>
           <div className='container'>
             <div className='row'>
-              <div className='col-8 grid-style'>
+              <div className= 'col-md-8 grid-style'>
                 8 of 12
               </div>
-              <div className='col-4 grid-style'>
+              <div className= 'col-md-4 grid-style'>
                 4 of 12
               </div>
             </div>
@@ -42,7 +51,7 @@ function App() {
           <p>Add your favorite bootstrap icon below</p>
         </div>
         <div className='section-controls-container'>
-          <span>Your icon goes here ➡️</span>
+          <span>Your icon goes here ➡️ <i className="bi bi-airplane-engines"></i></span>
         </div>
       </div>
     </div>
